115c2bf494f4d2cbeb8b3117314ee838e3935bed
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-06-20 22:35:10 -0700

    New stuffs

---
+++ modules/icinga/manifests/init.pp
@@ -0,0 +1,87 @@
+class icinga {
+
+  apt::key { 'icinga':
+    id     => 'F51A91A5EE001AA5D77D53C4C6E319C334410682',
+    source => 'http://packages.icinga.org/icinga.key', 
+  }
+
+  apt::source { 'icinga':
+    location => 'http://packages.icinga.org/ubuntu/',
+    release  => 'icinga-trusty-snapshots',
+    repos    => 'main',
+  }
+
+  Exec['apt_update'] -> Package <||>
+
+  apache::mod { 'rewrite': }
+
+  mysql::db { 'icinga2_data':
+    user     => 'icinga2',
+    password => 'icinga2',
+  }
+
+  mysql::db { 'icinga2_web':
+    user     => 'icinga2',
+    password => 'icinga2',
+  }
+
+  class { 'apache':
+    mpm_module => 'prefork',
+  }
+
+  class {'::apache::mod::php': }
+
+  class { 'apt':
+    update => {
+      frequency => 'daily',
+    },
+  }
+
+  class { 'icinga2':
+    manage_repos     => false,
+    db_user          => 'root',
+    db_pass          => 'password',
+    default_features => [
+      'checker',
+      'command',
+      'mainlog',
+      'notification',
+      'graphite',
+    ],
+  }
+
+  class { 'icinga2::feature::ido_mysql': }
+
+  class { 'icingaweb2':
+    config_dir_mode     => '0775',
+    install_method      => 'package',
+    pkg_repo_version    => 'snapshot',
+    manage_apache_vhost => true,
+    ido_db_name         => 'icinga2_data',
+    ido_db_user         => 'root',
+    ido_db_pass         => 'password',
+    web_db_name         => 'icinga2_web',
+    web_db_user         => 'root',
+    web_db_pass         => 'password',
+  }
+
+  class { 'mysql::server':
+    root_password           => 'password',
+    remove_default_accounts => true,
+  }
+
+  file { '/etc/icingaweb2/modules/monitoring/backends.ini':
+    ensure  => present,
+    owner   => 'icingaweb2',
+    group   => 'icingaweb2',
+    content => template('icinga/backends.ini'),
+  }
+
+  file { '/etc/icingaweb2/modules/monitoring/instances.ini':
+    ensure  => present,
+    owner   => 'icingaweb2',
+    group   => 'icingaweb2',
+    content => template('icinga/instances.ini'),
+  }
+
+}
