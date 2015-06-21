6627d883e28b4fe026f4917093e1dc57a6f00deb
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-06-20 23:42:30 -0700

    Fancy

--- modules/icinga/manifests/init.pp
+++ modules/icinga/manifests/init.pp
@@ -1,5 +1,10 @@
-class icinga {
+class icinga(
+  $db_type     = 'mysql',
+  $db_user     = 'root',
+  $db_password = 'password',
+) {
 
+  ### CUSTOM APT SETUP ###
   apt::key { 'icinga':
     id     => 'F51A91A5EE001AA5D77D53C4C6E319C334410682',
     source => 'http://packages.icinga.org/icinga.key', 
@@ -13,38 +18,54 @@ class icinga {
 
   Exec['apt_update'] -> Package <||>
 
-  apache::mod { 'rewrite': }
+  ### MYSQL SPECIFIC ###
+  if $db_type == 'mysql' {
 
-  mysql::db { 'icinga2_data':
-    user     => 'icinga2',
-    password => 'icinga2',
-    sql      => '/usr/share/icinga2-ido-mysql/schema/mysql.sql',
-    require  => Package['icinga2'],
+    class { 'mysql::server':
+      root_password           => 'password',
+      remove_default_accounts => true,
+    }
+
+    class { 'icinga2::feature::ido_mysql': }
+
+    mysql::db { 'icinga2_data':
+      user     => 'icinga2',
+      password => 'icinga2',
+      sql      => '/usr/share/icinga2-ido-mysql/schema/mysql.sql',
+      require  => Package['icinga2'],
+    }
+
+    mysql::db { 'icinga2_web':
+      user     => 'icinga2',
+      password => 'icinga2',
+      sql      => '/usr/share/icingaweb2/etc/schema/mysql.schema.sql',
+      require  => Package['icingaweb2'],
+    }
+
   }
 
-  mysql::db { 'icinga2_web':
-    user     => 'icinga2',
-    password => 'icinga2',
-    sql      => '/usr/share/icingaweb2/etc/schema/mysql.schema.sql',
-    require  => Package['icingaweb2'],
-  }
-
+  ### APACHE CONFIG ###
   class { 'apache':
     mpm_module => 'prefork',
   }
 
+  apache::mod { 'rewrite': }
+
   class {'::apache::mod::php': }
 
+  ### APT CONFIG ###
   class { 'apt':
     update => {
       frequency => 'daily',
     },
   }
 
+  ### ICINGA 2 CONFIG ###
   class { 'icinga2':
     manage_repos     => false,
-    db_user          => 'root',
-    db_pass          => 'password',
+    db_type          => $db_type,
+    db_user          => $db_user,
+    db_pass          => $db_password,
     default_features => [
       'checker',
       'command',
@@ -54,24 +75,20 @@ class icinga {
     ],
   }
 
-  class { 'icinga2::feature::ido_mysql': }
-
+  ### ICINGA WEB 2 CONFIG ###
   class { 'icingaweb2':
     config_dir_mode     => '0775',
     install_method      => 'package',
     pkg_repo_version    => 'snapshot',
     manage_apache_vhost => true,
+    ido_db              => $db_type,
     ido_db_name         => 'icinga2_data',
-    ido_db_user         => 'root',
-    ido_db_pass         => 'password',
+    ido_db_user         => $db_user,
+    ido_db_pass         => $db_password,
+    web_db              => $db_type,
     web_db_name         => 'icinga2_web',
-    web_db_user         => 'root',
-    web_db_pass         => 'password',
-  }
-
-  class { 'mysql::server':
-    root_password           => 'password',
-    remove_default_accounts => true,
+    web_db_user         => $db_user,
+    web_db_pass         => $db_password,
   }
 
   file { '/etc/icingaweb2/modules/monitoring':
@@ -84,6 +101,7 @@ class icinga {
 
   file { '/etc/icingaweb2/modules/monitoring/backends.ini':
     ensure  => present,
+    mode    => '0664',
     owner   => 'icingaweb2',
     group   => 'icingaweb2',
     content => template('icinga/backends.ini'),
@@ -91,6 +109,7 @@ class icinga {
 
   file { '/etc/icingaweb2/modules/monitoring/instances.ini':
     ensure  => present,
+    mode    => '0664',
     owner   => 'icingaweb2',
     group   => 'icingaweb2',
     content => template('icinga/instances.ini'),
