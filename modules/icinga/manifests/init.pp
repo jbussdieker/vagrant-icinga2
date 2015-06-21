2f788501b576b3735ca62fc5577f9571d45d6964
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-06-20 23:58:59 -0700

    Getting real

--- modules/icinga/manifests/init.pp
+++ modules/icinga/manifests/init.pp
@@ -4,44 +4,17 @@ class icinga(
   $db_password = 'password',
 ) {
 
-  ### CUSTOM APT SETUP ###
-  apt::key { 'icinga':
-    id     => 'F51A91A5EE001AA5D77D53C4C6E319C334410682',
-    source => 'http://packages.icinga.org/icinga.key', 
-  }
-
-  apt::source { 'icinga':
-    location => 'http://packages.icinga.org/ubuntu/',
-    release  => 'icinga-trusty-snapshots',
-    repos    => 'main',
-  }
-
   Exec['apt_update'] -> Package <||>
 
-  ### MYSQL SPECIFIC ###
-  if $db_type == 'mysql' {
-
-    class { 'mysql::server':
-      root_password           => 'password',
-      remove_default_accounts => true,
-    }
-
-    class { 'icinga2::feature::ido_mysql': }
-
-    mysql::db { 'icinga2_data':
-      user     => 'icinga2',
-      password => 'icinga2',
-      sql      => '/usr/share/icinga2-ido-mysql/schema/mysql.sql',
-      require  => Package['icinga2'],
-    }
-
-    mysql::db { 'icinga2_web':
-      user     => 'icinga2',
-      password => 'icinga2',
-      sql      => '/usr/share/icingaweb2/etc/schema/mysql.schema.sql',
-      require  => Package['icingaweb2'],
-    }
-
+  ### CUSTOM APT SETUP ###
+  apt::source { 'icinga':
+    location   => 'http://packages.icinga.org/ubuntu/',
+    release    => 'icinga-trusty-snapshots',
+    repos      => 'main',
+    key        => {
+      id     => 'F51A91A5EE001AA5D77D53C4C6E319C334410682',
+      source => 'http://packages.icinga.org/icinga.key',
+    },
   }
 
   ### APACHE CONFIG ###
@@ -119,6 +92,41 @@ class icinga(
     ensure  => link,
     target  => '/usr/share/icingaweb2/modules/monitoring',
     require => Package['icingaweb2'],
+  }
+
+  ### MYSQL SPECIFIC ###
+  if $db_type == 'mysql' {
+
+    class { 'mysql::server':
+      root_password           => 'password',
+      remove_default_accounts => true,
+    }
+
+    class { 'icinga2::feature::ido_mysql': }
+
+    mysql::db { 'icinga2_data':
+      user     => 'icinga2',
+      password => 'icinga2',
+      sql      => '/usr/share/icinga2-ido-mysql/schema/mysql.sql',
+      require  => Package['icinga2'],
+    }
+
+    mysql::db { 'icinga2_web':
+      user     => 'icinga2',
+      password => 'icinga2',
+      sql      => '/usr/share/icingaweb2/etc/schema/mysql.schema.sql',
+      require  => Package['icingaweb2'],
+      notify   => Exec['create_web_user'],
+    }
+
+    $default_user = 'icingaadmin'
+    $default_password = '\$1\$usghGMNC\$h8Lk.QA8OUSvEkz45o.0u1'
+
+    exec { 'create_web_user':
+      command     => "/usr/bin/mysql -u ${db_user} -p${db_password} icinga2_web -e \"insert into icingaweb_user VALUES ('${default_user}', 1, '${default_password}', NULL, NULL);\"",
+      refreshonly => true,
+    }
+
   }
 
 }
